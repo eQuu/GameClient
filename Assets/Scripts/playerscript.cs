d50d3ec3bf1fcd6a18920057f9ed830e7df639d0
eQuu <Alexander.vdPoel@web.de>
2016-11-27 14:45:50 +0100

    Github & Unity

---
+++ Assets/Scripts/playerscript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class playerscript : NetworkBehaviour {
+
+	public float moveSpeed = 0.03f;
+	private Rigidbody2D myBody;
+	// Use this for initialization
+	void Start () {
+		myBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isLocalPlayer) {
+			checkMovement ();
+		}
+	}
+
+	private void checkMovement() {
+		transform.position = new Vector2 ((transform.position.x + Input.GetAxis ("Horizontal") * moveSpeed),transform.position.y + Input.GetAxis ("Vertical") * moveSpeed);;
+	}
+}
