6a35a8576542c06261425380960dd04d0c2fd812
Curaw <herrkison@gmail.com>
2016-12-07 01:49:27 +0100

    Targeting System
    Man kann jetzt auf player raufklicken du huso

--- Assets/Scripts/playerscript.cs
+++ Assets/Scripts/playerscript.cs
@@ -4,12 +4,20 @@ using UnityEngine.Networking;
 
 public class playerscript : NetworkBehaviour {
 
-	public float moveSpeed = 0.03f;
+    public float moveSpeed = 0.03f;
+
 	private Rigidbody2D myBody;
-	// Use this for initialization
-	void Start () {
-		myBody = GetComponent<Rigidbody2D>();
-	}
+    private GameObject myTarget;
+    private gamescript myGameScript;
+
+
+    // Use this for initialization
+    void Start() {
+        myBody = GetComponent<Rigidbody2D>();
+        GameObject myGame = GameObject.Find("Game");
+        myGameScript = myGame.GetComponent<gamescript>();
+        myGameScript.setLocalPlayer(gameObject);
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -17,6 +25,11 @@ public class playerscript : NetworkBehaviour {
 			checkMovement ();
 		}
 	}
+
+    void OnMouseDown()
+    {
+        myGameScript.newTarget(gameObject);
+    }
 
 	private void checkMovement() {
 		transform.position = new Vector2 ((transform.position.x + Input.GetAxis ("Horizontal") * moveSpeed),transform.position.y + Input.GetAxis ("Vertical") * moveSpeed);;
