515778938e06e2e75cbb45422e554077b07f57a6
Curaw <herrkison@gmail.com>
2016-12-09 03:05:19 +0100

    Player
    Boden eingebaut und einen steuerbaren Spieler

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float movespeed;
+    private Rigidbody myBody;
+
+	// Use this for initialization
+	void Start () {
+        myBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        myBody.AddForce(movement * movespeed);
+    }
+}
