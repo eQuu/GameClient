f8e1adaa14888f5edf3f2bdcdeba1871c576fd97
Curaw <herrkison@gmail.com>
2016-12-28 17:14:23 +0100

    Aufgeräumt
    Ordnerstruktur verbessert

---
+++ Assets/Scripts/playerScript.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerScript : MonoBehaviour {
+
+    public float movespeed;
+    public cameraScript myCameraScript;
+    public uiScript myUiScript;
+
+    private float playerInputVertical;
+    private float playerInputHorizontal;
+    private float playerInputQundE;
+    private float playerInputJump;
+    private RaycastHit clicked;
+    private Ray myRay;
+
+    private Rigidbody myBody;
+
+    // Use this for initialization
+    void Start () {
+        myBody = GetComponent<Rigidbody>();
+	}
+
+    private bool isGrounded()
+    {
+        return Physics.Raycast(transform.position, -Vector3.up, 0.1f);
+    }
+
+    void Update()
+    {
+        playerInputVertical = Input.GetAxis("Vertical");
+        playerInputHorizontal = Input.GetAxis("Horizontal");
+        playerInputQundE = Input.GetAxis("QundE");
+        playerInputJump= Input.GetAxis("Jump");
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(myRay, out clicked))
+            {
+                if (clicked.transform.tag.Contains("trgt"))
+                {
+                    myUiScript.setTarget(clicked.transform);
+                } else
+                {
+                    myUiScript.setTarget(null);
+                }
+            } else
+            {
+                myUiScript.setTarget(null);
+            }
+        }
+
+        if (playerInputJump > 0 && isGrounded())
+        {
+            myBody.AddForce(Vector3.up * 40);
+        }
+
+        if (playerInputQundE < 0)
+        {
+            myBody.transform.position = myBody.transform.position + transform.right * movespeed * Time.deltaTime;
+        } else if (playerInputQundE > 0)
+        {
+            myBody.transform.position = myBody.transform.position - transform.right * movespeed * Time.deltaTime;
+        }
+
+        if (playerInputHorizontal != 0)
+        {
+            transform.Rotate(Vector3.up, playerInputHorizontal);
+            myCameraScript.followRotation(playerInputHorizontal);
+
+        }
+        if (playerInputVertical > 0)
+        {
+            myBody.transform.position = myBody.transform.position + transform.forward * movespeed * Time.deltaTime;
+        } else if (playerInputVertical < 0)
+        {
+            myBody.transform.position = myBody.transform.position - transform.forward * movespeed * Time.deltaTime;
+        }
+    }
+}
